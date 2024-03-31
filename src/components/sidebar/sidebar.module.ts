261607f80600c798517b33be16def5144148071e
David Capa <capaseitadavid@gmail.com>
2024-03-31 22:33:41 +0100

    Add date range search functionality to sidebar component

--- src/components/sidebar/sidebar.module.ts
+++ src/components/sidebar/sidebar.module.ts
@@ -5,14 +5,19 @@ import { SidebarComponent } from './sidebar.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MapsModule } from '../maps/maps.module';
 import { ContentComponent } from './content/content.component';
-
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
 @NgModule({
   declarations: [SidebarComponent, ContentComponent],
   imports: [
     CommonModule,
     MatSidenavModule,
     BrowserAnimationsModule,
-    MapsModule
+    MapsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
   ],
   exports: [SidebarComponent]
 })
